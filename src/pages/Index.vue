4f6c84157eeb1257cf0b2aa740f758f3344d4519
raulhess <raul.hess@preambulo.com.br>
2019-11-19 12:35:44 -0300

    [dev] adicionado tratamento para a store e adicionados os filtros de ordenação

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,25 @@
 <template>
   <q-page class="row reverse">
-    <div class="col-auto" style="width: 848px;margin-top: 55px;">
+    <div class="col-auto bg-grey-2" style="width: 100%;max-width: 848px;margin-top: 55px;">
+
+      <!-- TOOLBAR -->
       <q-toolbar style="margin-top: -13px;">
-        <q-toolbar-title  class="games-toolbar">Games</q-toolbar-title>
-        <q-select v-model="orderBy" :options="orderByOptions" emit-value map-options dense outlined></q-select>
+        <q-toolbar-title  class="games-toolbar" >Games</q-toolbar-title>
+        <q-select v-model="orderBy" :options="orderByOptions" emit-value map-options dense outlined style="width: 175px;"></q-select>
       </q-toolbar>
+
+      <!-- CONTAINER DOS PRODUTOS -->
+      <div id="product-container">
+        
+      </div>
+
     </div>
   </q-page>
 </template>
 
 <script>
+import {mapState} from 'vuex'
+
 export default {
 
   name: 'PageIndex',
@@ -18,9 +28,38 @@ export default {
     return {
       orderBy: 'score',
       orderByOptions: [
-        {value:'score',label:'Mais populares'}
+        {value:'score',label:'Mais populares'},
+        {value:'priceLow',label:'Menores preços'},
+        {value:'priceHigh',label:'Maiores preços'},
+        {value:'alphabetically',label:'Ordem alfabética'}
       ]
     }
+  },
+
+  computed: {
+    ...mapState('gamesModule',['products']),
+
+    orderedProductList: function () {
+      // retorna uma cópia da lista original de produtos ordenada de acordo com a variável [orderBy]
+      switch(this.orderBy){
+        case 'score':
+          return [...this.products].sort((x,y) => (x.score > y.score) ? -1 : 1)
+        case 'priceLow':
+          return [...this.products].sort((x,y) => (x.price < y.price) ? -1 : 1)
+        case 'priceHigh':
+          return [...this.products].sort((x,y) => (x.price > y.price) ? -1 : 1)
+        case 'alphabetically':
+          return [...this.products].sort((x,y) => (x.name.toUpperCase() < y.name.toUpperCase()) ? -1 : 1)
+      }
+      // caso não haja um valor correto na variável [orderBy], retorna uma cópia da lista original
+      return [...this.products]
+    },
+  },
+
+  beforeMount() {
+    // carrega a lista de produtos do JSON e envia para a store
+    let json = require('../products.json')
+    this.$store.dispatch('gamesModule/setList',{products:json})
   }
 
 }
@@ -30,7 +69,7 @@ export default {
 
   .games-toolbar {
     color: #363636;
-    height: 65px;
+    line-height: 65px;
     font-weight: 600;
     font-size: 48px;
   }
